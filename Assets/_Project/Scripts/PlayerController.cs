cc5281db0da9f86c3b60e03d2f6961c7bf1d9a52
Andrea Fraboni <151090404+AndreaFraboni@users.noreply.github.com>
2026-01-06 11:51:15 +0100

    PlayerController move and rotation in the direction

--- Assets/_Project/Scripts/PlayerController.cs
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float _speed = 5f;
-
     private Rigidbody _rb;
     private float horizontal, vertical;
-
     private Vector3 move;
 
     private void Awake()
@@ -24,21 +22,26 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
+        Rotation();
     }
-
 
     private void CheckInput()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxisRaw("Horiz" +
+            "ontal");
         vertical = Input.GetAxisRaw("Vertical");
 
         move = new Vector3(horizontal, 0, vertical);
-
     }
 
     private void Move()
     {
         _rb.MovePosition(transform.position + move * Time.deltaTime);
+    }
+
+    private void Rotation()
+    {
+        if (move != Vector3.zero) transform.forward = move; // Simple Fix For “Look Rotation Viewing Vector Is Zero”
     }
 
 }
